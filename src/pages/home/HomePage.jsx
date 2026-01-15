d6e8e42fd8c648f8d8037afa749f4a7d5da55caa
Vilde Avløs <vildeavlos@gmail.com>
2026-01-15 17:23:14 +0100

    Moved search bar into home page only

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getProducts } from "../../api/getProducts";
+import SearchBar from "../../components/products/search-bar/SearchBar";
 import ProductCard from "../../components/products/product-card/ProductCard";
 import s from "./HomePage.module.scss";
 
@@ -17,6 +18,9 @@ export default function HomePage() {
   return (
     <section className={s.container}>
       <h1>Products</h1>
+      <div className={s["search-wrapper"]}>
+        <SearchBar />
+      </div>
 
       <ul className={s.grid}>
         {products.map((product) => (
